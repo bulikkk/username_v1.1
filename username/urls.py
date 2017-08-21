ae570c59448061c5d1bd623de3775aadfc324673
Piotrek Bulik <bulik.piotr@gmail.com>
2017-08-21 18:37:12 +0200

    added mysql and first template

--- username/urls.py
+++ username/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from username_app.views import MainView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', MainView.as_view(), name='main'),
 ]
